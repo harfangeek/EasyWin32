2903140a5f3f1ac7a4f2ff2ff13b68418ae1f024
unknown <harfangeek@gmail.com>
2014-12-02 22:25:44 +0100

    Add test projects

---
+++ Registry/test/ListValues/main.c
@@ -0,0 +1,105 @@
+#include <windows.h>
+#include <wchar.h>
+
+#define VALUE_NAME_LEN 512
+#define VALUE_TYPE_LEN 32
+
+void listKeyValues(HKEY Key);
+void getValueTypeStr(DWORD valueType, LPWSTR valueTypeStr);
+
+int wmain(int argc, wchar_t **argv)
+{
+	PDWORD cChars = NULL;
+	HANDLE std = GetStdHandle(STD_OUTPUT_HANDLE);
+	HKEY hKey;
+	LSTATUS res;
+
+	WCHAR keyClass[1024];
+	DWORD keyClassLen = 1024;
+	DWORD lst;
+
+	// Ouverture de la clé
+	res = RegOpenKeyExW(HKEY_CURRENT_USER, L"Test", 0, KEY_EXECUTE, &hKey);
+	if (res != ERROR_SUCCESS)
+	{
+		wprintf(L"Impossible d'ouvrir la cl\x82\n");
+		system("pause");
+		return EXIT_FAILURE;
+	}
+
+	listKeyValues(hKey);
+	/*lst = RegQueryInfoKeyW(hKey, keyClass, &keyClassLen, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+	wprintf(L"%s", keyClass);
+
+	RegQueryInfoKeyW(hKey, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, &lst, NULL);*/
+
+	RegCloseKey(hKey); // Fermeture de la clé
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
+
+void listKeyValues(HKEY hKey)
+{
+	LONG res;
+	DWORD i = 0;
+	WCHAR valueName[VALUE_NAME_LEN];
+	DWORD valueNameLen;
+	DWORD valueType;
+	WCHAR valueTypeStr[VALUE_TYPE_LEN];
+
+	do
+	{
+		valueNameLen = VALUE_NAME_LEN; // Besoin de remettre la longueur du nom car elle est changé par RegEnumValue
+		res = RegEnumValueW(hKey, i++, valueName, &valueNameLen, 0, &valueType, NULL, NULL); // On pourrait récupéré la donnée en même temps via les deux derniers paramètres
+		if (res == ERROR_SUCCESS)
+		{
+			valueName[valueNameLen] = '\0'; // Au cas où, la présence du null byte n'est pas certifié
+			getValueTypeStr(valueType, valueTypeStr); // On récupère le type de la valeur sous forme d'une string
+			wprintf(L"Name: %s\tSize: %d\tType: %s\n", valueNameLen > 0 ? valueName : L"(default)", valueNameLen, valueTypeStr);
+		}
+	} while (res == ERROR_SUCCESS);
+
+	if (res == ERROR_NO_MORE_ITEMS)
+		wprintf(L"\n#END#\n");
+	else
+		wprintf(L"\n#ERROR#\n");
+}
+
+// Renvoie le type de valeur correspondant à valueType sous la forme d'une string
+void getValueTypeStr(DWORD valueType, LPWSTR valueTypeStr)
+{
+	switch (valueType)
+	{
+	case REG_BINARY:
+		lstrcpyW(valueTypeStr, L"REG_BINARY");
+		break;
+	case REG_DWORD:
+		lstrcpyW(valueTypeStr, L"REG_DWORD");
+		break;
+	case REG_DWORD_BIG_ENDIAN:
+		lstrcpyW(valueTypeStr, L"REG_DWORD_BIG_ENDIAN");
+		break;
+	case REG_EXPAND_SZ:
+		lstrcpyW(valueTypeStr, L"REG_EXPAND_SZ");
+		break;
+	case REG_LINK:
+		lstrcpyW(valueTypeStr, L"REG_LINK");
+		break;
+	case REG_MULTI_SZ:
+		lstrcpyW(valueTypeStr, L"REG_MULTI_SZ");
+		break;
+	case REG_NONE:
+		lstrcpyW(valueTypeStr, L"REG_NONE");
+		break;
+	case REG_QWORD:
+		lstrcpyW(valueTypeStr, L"REG_QWORD");
+		break;
+	case REG_SZ:
+		lstrcpyW(valueTypeStr, L"REG_SZ");
+		break;
+	default:
+		lstrcpyW(valueTypeStr, L"OTHER");
+	}
+}
---
+++ Registry/test/ReadValues/main.c
@@ -0,0 +1,110 @@
+#include <windows.h>
+#include <wchar.h>
+
+HKEY GetOpenKey(HKEY hKey, wchar_t  * SubKey)
+{
+	//wchar_t * lpSubKey = SubKey; // Pas utile, on peut passer directement SubKey à RegOpenKeyExW
+	HKEY phkResult;
+	long lResult;
+
+	wprintf(L"Ouverture de la cl\x82 : %ls\n\n", SubKey);
+
+	lResult = RegOpenKeyExW(hKey, SubKey, 0, KEY_READ, &phkResult);
+	if (lResult != ERROR_SUCCESS)
+	{
+		wprintf(L"Impossible d'ouvrir la cl\x82\n");
+		return NULL; // La fonction doit retourner une valeur dans tous les cas
+	}
+	else
+		return phkResult;
+}
+
+DWORD GetValueType(HKEY Key, wchar_t  * valueName) // DWORD à la place de int, histoire de garder les types Windows, renommé en GetValueType
+{
+	DWORD dwType;
+	//DWORD dwSize; // Pas besoin de récupérer dwSize
+
+	wprintf(L"Calcule du Type de la valeur : %ls \n\n", valueName);
+
+	if (RegQueryValueExW(Key, valueName, NULL, &dwType, NULL,/*&dwSize*/NULL) == ERROR_SUCCESS)
+	{
+		wprintf(L"\t dwType : %ld \n\n", dwType);
+		return dwType;
+	}
+	else
+		return -1; // La fonction doit retourner une valeur dans tous les cas
+}
+
+DWORD GetValueSize(HKEY Key, wchar_t  * SubKey) // DWORD à la place de int, histoire de garder les types Windows, renommé en GetValueSize
+{
+	DWORD dwSize;
+	// DWORD dwType; // Pas besoin de récupérer le type
+
+	wprintf(L"Calcule de la taille de la valeur : %ls \n\n", SubKey);
+
+	if (RegQueryValueExW(Key, SubKey, NULL,/*&dwType*/NULL, NULL, &dwSize) == ERROR_SUCCESS)
+	{
+		wprintf(L"\t dwSize : %ld \n\n", dwSize);
+		return dwSize;
+	}
+	else
+		return -1; // La fonction doit retourner une valeur dans tous les cas
+}
+
+BYTE * SetKeyDataSize(int dwSize)
+{
+	BYTE *lpData = NULL;
+
+	wprintf(L"Allocation de la memoire pour la valeur :\n\n");
+	lpData = (PBYTE)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, dwSize);
+	wprintf(L"\t lpData en : %ld \n", sizeof(lpData));
+
+	return lpData;
+}
+
+void GetDwordValue(HKEY Key, wchar_t * lpValueName, BYTE * lpData) // Renommé en GetDwordValue
+{
+	DWORD dwSize;
+	DWORD dwType;
+
+	long lResult = RegQueryValueExW(Key, lpValueName, NULL, &dwType, lpData, &dwSize);
+	DWORD *pDwData = (DWORD*)lpData;
+	DWORD dwData = *pDwData;
+	wprintf(L"[?] Code Retour du Calcul de lpData: %ld \n\n", lResult);
+	wprintf(L"\tValeur de la cl\x82 %s: %lu \n", lpValueName, dwData);
+}
+
+void GetSZValue(HKEY Key, wchar_t * lpValueName, byte * lpData) // Renommé en GetSZValue
+{
+	DWORD dwSize;
+	DWORD dwType;
+	long lResult = RegQueryValueExW(Key, lpValueName, NULL, &dwType, lpData, &dwSize);
+	wchar_t *pSzData = (wchar_t*)lpData;
+
+	wprintf(L"[?] Code Retour du Calcul de lpData: %ld \n\n", lResult);
+	wprintf(L"\tValeur de la cl\x82 %s: %ls \n", lpValueName, pSzData);
+}
+
+int wmain(int argc, wchar_t** argv)
+{
+	HKEY hKey;
+	DWORD valueType, valueSize;
+	BYTE* pData;
+
+	hKey = GetOpenKey(HKEY_CURRENT_USER, L"Test");
+
+	valueType = GetValueType(hKey, L"value32");
+	valueSize = GetValueSize(hKey, L"value32");
+	pData = SetKeyDataSize(valueSize);
+	GetDwordValue(hKey, L"value32", pData);
+	HeapFree(GetProcessHeap(), 0, pData);
+
+	valueType = GetValueType(hKey, L"str1");
+	valueSize = GetValueSize(hKey, L"str1");
+	pData = SetKeyDataSize(valueSize);
+	GetSZValue(hKey, L"str1", pData);
+	HeapFree(GetProcessHeap(), 0, pData);
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
