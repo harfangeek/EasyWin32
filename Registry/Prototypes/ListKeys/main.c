109602ea9a8254f4591324cf09b1e9bfb0b0d200
unknown <harfangeek@gmail.com>
2015-02-19 16:07:45 +0100

    Reorganize prototypes folder New prototype for listing keys

---
+++ Registry/Prototypes/ListKeys/main.c
@@ -0,0 +1,48 @@
+#include <windows.h>
+#include <wchar.h>
+
+#define KEY_NAME_LEN 255
+#define KEY_CLASS_LEN 255
+
+void listKeys(HKEY hKey);
+
+int wmain(int argc, wchar_t **argv)
+{
+	HANDLE std = GetStdHandle(STD_OUTPUT_HANDLE);
+	HKEY hKey;
+	LSTATUS res;
+
+	res = RegOpenKeyExW(HKEY_CURRENT_USER, L"Software", 0, KEY_EXECUTE, &hKey);
+	if (res != ERROR_SUCCESS)
+	{
+		wprintf(L"Impossible d'ouvrir la cl\x82\n");
+		system("pause");
+		return EXIT_FAILURE;
+	}
+
+	listKeys(hKey);
+
+	RegCloseKey(hKey); // Fermeture de la clé
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
+
+void listKeys(HKEY hKey)
+{
+	DWORD i = 0;
+	WCHAR keyName[KEY_NAME_LEN];
+	DWORD keyNameLen = KEY_NAME_LEN;
+	WCHAR keyClass[KEY_CLASS_LEN];
+	DWORD keyClassLen = KEY_CLASS_LEN;
+	LSTATUS res;
+
+	while((res = RegEnumKeyExW(hKey, i, keyName, &keyNameLen, 0, keyClass, &keyClassLen, NULL)) == ERROR_SUCCESS)
+	{
+		wprintf(L"Name: %s\tClass: %s\n", keyName, keyClass);
+		i++;
+		keyNameLen = KEY_NAME_LEN;
+		keyClassLen = KEY_CLASS_LEN;
+	}
+}
--- Registry/main.c
+++ Registry/main.c
@@ -1,6 +1,16 @@
 #include <windows.h>
 #include <wchar.h>
 
+/*
+Registry Element	Size Limit
+Key name			255 characters. The key name includes the absolute path of the key in the registry, always starting at a base key, for example, HKEY_LOCAL_MACHINE.
+Value name			16,383 characters
+					Windows 2000:  260 ANSI characters or 16,383 Unicode characters.
+Value				Available memory (latest format)
+					1 MB (standard format)
+Tree				A registry tree can be 512 levels deep. You can create up to 32 levels at a time through a single registry API call.
+*/
+
 int wmain(int argc, wchar_t** argv)
 {
 
